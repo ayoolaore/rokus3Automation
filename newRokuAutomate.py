6817891412676c5d1ff3d74dfcd0225d7b9eba57
ayoola <ayoola2012@gmail.com>
2021-03-31 11:03:13 -0500

    upload to s3 complete, working on delete

--- newRokuAutomate.py
+++ newRokuAutomate.py
@@ -11,6 +11,18 @@ titles = []
 dataList = {}
 data = ""
 stringWord = ""
+'''
+read keys in 
+'''
+accessKeysfile = open("accessKey.txt", "r")
+secretKeysfile = open("secretKey.txt", "r")
+
+access_key_id = accessKeysfile.read()
+secret_key_id = secretKeysfile.read()
+
+accessKeysfile.close()
+secretKeysfile.close()
+
 
 def yesno():
     """Simple Yes/No Function."""
@@ -24,9 +36,10 @@ def yesno():
     if ans == 'd':
         ans2 = input("Are you sure you want to delete an item y/n \n")
         if ans2 == 'y':
+
             deleteTitle = input("Enter Title you would like deleted")
-            dataList.pop(deleteTitle, None)
-            print(f'{dataList.pop(deleteTitle,None)} has been deleted from library')
+
+            deleteContent(deleteTitle)
         else:
             yesno()
     if ans == 'exit':
@@ -108,6 +121,7 @@ def jsonProcess():
     f = open("data.json", "w")
     f.write(data)
     f.close()
+    return data
     #data = json.loads(data)
     #print(data)
 # def addContent():
@@ -115,12 +129,32 @@ def jsonProcess():
 # if context == True:
 # content()
 
+def deleteContent(deleteTitle):
+    with open("data.json", 'w') as file:
+        dataFile = json.load(file)
+        result = dataFile.pop(deleteTitle, None)
+        print(f"{result} is now deleted from channel")
+
+def uploadContent():
+    s3 = boto3.resource('s3',
+                        aws_access_key_id=access_key_id,
+                        aws_secret_access_key=secret_key_id)
+    s3object = s3.Object('rokucotr', 'Json Files/data.json')
+
+    s3object.put(
+        Body=(bytes(jsonProcess().encode('UTF-8'))),
+        ContentType='application/json',
+        ACL='public-read'
+    )
+    print("file merged and uploaded to S3, SUCESS!!!")
+
 
 
 def main():
     grabFiles()
     #stringWord = parseThrough()
-    jsonProcess()
+    #jsonProcess()
+    uploadContent()
     #print(stringWord)
     #print(urls)
     #print(titles)
